b495b8cc73963f1c530bd2354027f92c0de78868
chinni00 <chinni0437@gmail>
2023-06-18 15:27:23 +0530

    used useEffect for calling api when page loads and also used useCallback for not unknown rerenders the page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect,useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,7 +8,9 @@ function App() {
   const [isLoading,setIsLoading] = useState(false);
   const [error,setError] = useState(null);
 
- const fetchMoviesHandler =  async () => {
+ 
+
+ const fetchMoviesHandler =   useCallback( async() => {
   setIsLoading(true);
   setError(null)
   try{
@@ -39,7 +41,11 @@ function App() {
      
   }
   setIsLoading(false)
-  };
+  },[]);
+  
+   useEffect(()=>{
+    fetchMoviesHandler();
+  },[fetchMoviesHandler])
 
   return (
     <React.Fragment>
